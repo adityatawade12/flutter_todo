9408e6925595346bcc191bcff7bc7299e21dd83a
Aditya <aditya.tawade@somaiya.edu>
2021-08-29 18:49:33 +0530

    final touchup

--- lib/widgets/newTaskModal.dart
+++ lib/widgets/newTaskModal.dart
@@ -54,88 +54,92 @@ class _TaskModalState extends State<TaskModal> {
   @override
   Widget build(BuildContext context) {
     final localizations = MaterialLocalizations.of(context);
-    return Padding(
-      padding: EdgeInsets.only(
-          bottom: MediaQuery.of(context).viewInsets.vertical * 0.5),
-      child: Card(
-        child: Container(
-          height: 300,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                TextField(
-                  decoration: InputDecoration(labelText: "Task"),
-                  controller: taskController,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: Text(
-                          _selectedDate == DateTime(1990)
-                              ? "No Date Chosen"
-                              : DateFormat.yMMMd().format(_selectedDate),
-                        ),
-                      ),
-                      ElevatedButton(
-                          onPressed: _presentDatePicker,
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.orange)),
-                          child: Text(
-                            "Choose Date",
-                            style: TextStyle(color: Colors.white),
-                          ))
-                    ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom * 0.5),
+        child: Card(
+          child: Container(
+            height: 300,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: "Task"),
+                    controller: taskController,
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: Text(
-                          _selectedTime == null
-                              ? "No Time Chosen"
-                              : localizations.formatTimeOfDay(_selectedTime),
-                        ),
-                      ),
-                      ElevatedButton(
-                          onPressed: _presentTimePicker,
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.orange)),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
                           child: Text(
-                            "Choose Time",
-                            style: TextStyle(color: Colors.white),
-                          ))
-                    ],
+                            _selectedDate == DateTime(1990)
+                                ? "No Date Chosen"
+                                : DateFormat.yMMMd().format(_selectedDate),
+                          ),
+                        ),
+                        ElevatedButton(
+                            onPressed: _presentDatePicker,
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.orange)),
+                            child: Text(
+                              "Choose Date",
+                              style: TextStyle(color: Colors.white),
+                            ))
+                      ],
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                ElevatedButton(
-                    onPressed: saveTask,
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.orange)),
-                    child: Text(
-                      "Save Task",
-                      style: TextStyle(color: Colors.white),
-                    )),
-                // if (MediaQuery.of(context).viewInsets.bottom > 0)
-                // SizedBox(
-                //   height: 400,
-                // )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Text(
+                            _selectedTime == null
+                                ? "No Time Chosen"
+                                : localizations.formatTimeOfDay(_selectedTime),
+                          ),
+                        ),
+                        ElevatedButton(
+                            onPressed: _presentTimePicker,
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.orange)),
+                            child: Text(
+                              "Choose Time",
+                              style: TextStyle(color: Colors.white),
+                            ))
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  ElevatedButton(
+                      onPressed: saveTask,
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.orange)),
+                      child: Text(
+                        "Save Task",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                  // if (MediaQuery.of(context).viewInsets.bottom > 0)
+                  // SizedBox(
+                  //   height: 400,
+                  // )
+                ],
+              ),
             ),
           ),
         ),
